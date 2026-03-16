2f267fd3a13ca017781b3556f6b21efdecf20794
ayushkumar1420 <ayushmaurya496@gmail.com>
2026-03-17 00:54:41 +0530

    css added

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import './home.css'
+import { PiStudentFill } from "react-icons/pi";
+import { GiTeacher } from "react-icons/gi";
+import { RiAdminFill } from "react-icons/ri";
 
-const home = () => {
+const Home = () => {
   return (
     <div class="ctn-1">
       <div className="badge">
@@ -9,8 +12,28 @@ const home = () => {
         <span>AI-Powered Attendance System</span>
         <span class="icon">✨</span>
       </div>
+
+      <h1 className='head'>Attendance-AI</h1>
+      <p className='para'>Face Recognition · Real-time Analytics · Smart Tracking</p>
+      <p className='option'>Select your role to continue</p>
+
+      <div className='roles'>
+        <div className='student'>
+          <span className='st-icons'><PiStudentFill /></span>
+          <h2>Student</h2>
+        </div>
+        <div className='teacher'>
+          <span className='te-icons'><GiTeacher /></span>
+          <h2>Teacher</h2>
+        </div>
+        <div className='admin'>
+          <span className='ad-icons'><RiAdminFill /></span>
+          <h2>Admin</h2>
+        </div>
+      </div>
+      
     </div>
   )
 }
 
-export default home
+export default Home
