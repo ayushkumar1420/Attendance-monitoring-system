8e088169ce8e3d6ee5098719c2fccfdab5a75896
ayushkumar1420 <ayushmaurya496@gmail.com>
2026-03-16 20:26:09 +0530

    add file

---
+++ frontend/src/components/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './home.css'
+
+const home = () => {
+  return (
+    <div class="ctn-1">
+      <div className="badge">
+        <span class="icon">⚡</span>
+        <span>AI-Powered Attendance System</span>
+        <span class="icon">✨</span>
+      </div>
+    </div>
+  )
+}
+
+export default home
